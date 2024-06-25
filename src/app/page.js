0503cba01f8290ff07777301cdf7874e6b3a3539
ryumei <ryumei.sakaki06@gmail.com>
2024-06-26 08:17:34 +0900

    都道府県セレクトボックス途中

--- src/app/page.js
+++ src/app/page.js
@@ -3,41 +3,64 @@ import React, { useEffect, useState } from "react";
 
 const InputSelectBox = () => {
   const [regions, setRegions] = useState([]);
-  const [selectedRegion, setSelectedRegion] = useState('')
+  const [prefs, setPrefs] = useState([]);
+  const [selectedRegion, setSelectedRegion] = useState('');
+  const [selectedPrefs, setSelectedPrefs] = useState('');
 
   useEffect(() => {
-    const fetchRegions = async () => {
+    (async () => {
       const res1 = await fetch(`https://www.jma.go.jp/bosai/common/const/area.json`);
       const areaJson = await res1.json();
       setRegions(areaJson.centers);
+      setPrefs(areaJson.offices);
       setSelectedRegion(Object.keys(areaJson.centers)[0]);
-    };
-    fetchRegions();
+      setSelectedPrefs(Object.keys(areaJson.offices)[0]);
+    })();
   }, []);
 
+  // selectedPrefsの初期値
+  useEffect(() => {
+    if (selectedRegion) {
+      const region = regions[selectedRegion];
+      if (region) {
+        const prefCode = region.children;
+        setSelectedPrefs(prefCode[0]);
+      }
+    }
+  }, [selectedRegion, regions]);
 
+  console.log(`selectedPrefs`, selectedPrefs);
   const handleRegionChange = (e) => {
     setSelectedRegion(e.target.value);
-  }
-  console.log(selectedRegion);
-  console.log(regions);
+  };
+  const handlePrefChange = (e) => {
+    setSelectedPrefs(e.target.value);
+  };
 
+  console.log(prefs);
   return (
     <>
       <h2>地域選択</h2>
       <select value={selectedRegion} onChange={handleRegionChange}>
         {Object.keys(regions).map((regionCode) => {
-          <option key={regionCode} value={regionCode}>
-            {regionCode}
-          </option>
+          return (<option key={regionCode} value={regionCode}>
+            {regions[regionCode].name}
+          </option>)
+        })}
+      </select>
+      <select value={selectedPrefs} onChange={handlePrefChange}>
+        {regions[selectedRegion].children.map((prefCode) => {
+          return (<option key={prefCode} value={prefCode}>
+            {prefs[prefCode].name}
+          </option>)
         })}
       </select>
     </>
-  )
-}
+  );
+};
 
 const Home = () => {
-  return (<InputSelectBox />)
-}
+  return <InputSelectBox />;
+};
 
 export default Home;
